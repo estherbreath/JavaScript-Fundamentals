bdcaf157c72912638031925150750d34176cac4d
estherbreath <estherojile248@gmail.com>
2022-12-26 10:14:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//Log to console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+console.table({a:1, b:2});
+console.error('thos is some error');
+console.clear();
+console.warn('This is a warning');
+console.time('Hello');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+console.timeEnd('Hello');
