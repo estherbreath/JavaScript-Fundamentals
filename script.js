7c668b8cbea4a7a4e7370d97365d33533cf7014c
estherbreath <estherojile248@gmail.com>
2023-01-03 09:18:58 +0100

    changes

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 //Log to console
-console.log('Hello World');
-console.log(123);
-console.log(true);
-console.table({a:1, b:2});
-console.error('thos is some error');
-console.clear();
-console.warn('This is a warning');
-console.time('Hello');
-    console.log('Hello world');
-    console.log('Hello world');
-    console.log('Hello world');
-    console.log('Hello world');
-    console.log('Hello world');
-    console.log('Hello world');
-console.timeEnd('Hello');
+// console.log('Hello World');
+// console.log(123);
+// console.log(true);
+// console.table({a:1, b:2});
+// console.error('thos is some error');
+// console.clear();
+// console.warn('This is a warning');
+// console.time('Hello');
+//     console.log('Hello world');
+//     console.log('Hello world');
+//     console.log('Hello world');
+//     console.log('Hello world');
+//     console.log('Hello world');
+//     console.log('Hello world');
+// console.timeEnd('Hello');
+
+
+//Variables
+//Init var (Initailizing variables)
+var greeting;
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting);
+
+//variable can have letters, numbers, _, $
+//can not start with a number
+
+
+//Multi word vars
+var firstName = 'John'; //Camel case
+var first_name = 'Sara'; //Underscore
+var FirstName = 'Tom'; //Pascal case
+var firstname;
+
