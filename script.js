7c2fc45c5017958b211f299b47c28b7573b44e3c
estherbreath <estherojile248@gmail.com>
2023-01-07 17:36:18 +0100

    changes

--- script.js
+++ script.js
@@ -164,63 +164,138 @@
 
 
 // Create some  arrays
-const numbers = [43,56,98,44,45,51,5];
-const numbers2 = new Array(22,45,33,76,54);
-const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
-const mixed = [22, 'Hello', true, undefined, null, {a:1, b:1}, new Date()];
+// const numbers = [43,56,98,44,45,51,5];
+// const numbers2 = new Array(22,45,33,76,54);
+// const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
+// const mixed = [22, 'Hello', true, undefined, null, {a:1, b:1}, new Date()];
 
-let val;
+// let val;
 
-// Get array length
-val = numbers.length;
-//check
-val = Array.isArray(numbers);//is to check if it's an array
-//Get single value
-val = numbers[3];
-val = numbers[0];
-// Insert into array
-numbers[2] = 100;
-//Find index of value
-val = numbers.indexOf(45);
+// // Get array length
+// val = numbers.length;
+// //check
+// val = Array.isArray(numbers);//is to check if it's an array
+// //Get single value
+// val = numbers[3];
+// val = numbers[0];
+// // Insert into array
+// numbers[2] = 100;
+// //Find index of value
+// val = numbers.indexOf(45);
 
-// MUTATING ARRAYS
-//Add on to end
-numbers.push(250);
-//Add on to front
-numbers.unshift(120);
-//Take off from end
-numbers.pop();
-//Take off from front
-numbers.shift();
-//Splice values
-numbers.splice(1,3) //the !st value is where is starts and the 2nd where it ends
-//Reverse
-numbers.reverse();
+// // MUTATING ARRAYS
+// //Add on to end
+// numbers.push(250);
+// //Add on to front
+// numbers.unshift(120);
+// //Take off from end
+// numbers.pop();
+// //Take off from front
+// numbers.shift();
+// //Splice values
+// numbers.splice(1,3) //the !st value is where is starts and the 2nd where it ends
+// //Reverse
+// numbers.reverse();
 
-// Concatenate array
-val = numbers.concat(numbers2);
+// // Concatenate array
+// val = numbers.concat(numbers2);
 
-// Sorting arrays
-val = fruit.sort();
-val = numbers.sort();
+// // Sorting arrays
+// val = fruit.sort();
+// val = numbers.sort();
 
-//Use the "compare function"
-val = numbers.sort(function(x, y){
-    return x - y;
-});
+// //Use the "compare function"
+// val = numbers.sort(function(x, y){
+//     return x - y;
+// });
 
-//Reverse sort
-val = numbers.sort(function(x, y){
-    return y - x;
-});
+// //Reverse sort
+// val = numbers.sort(function(x, y){
+//     return y - x;
+// });
 
-Find
-function under50(num){
-    return num < 50;
-}
+// Find
+// function under50(num){
+//     return num < 50;
+// }
 
-val = numbers.find(under50);
+// val = numbers.find(under50);
 
 
-console.log(numbers);
+// console.log(numbers);
+// console.log(val);
+
+
+
+//OBJECT LITERALS
+ const person = {
+    firstNme: 'Steve',
+    lastName: 'Smith',
+    age: 30,
+    email: 'steve@aol.com',
+    hobbies: ['music', 'sport'],
+    address: {
+      city: 'Miami',
+      state: 'FL'
+    },
+    getBirthYear: function(){
+      return 2017 - this.age;
+    }
+
+ }
+
+ let val;
+
+ val = person;
+ //Get specific value
+ val = person.firstNme;
+ val = person['firstNme'];
+ val = person.age;
+ val = person.hobbies[1];
+ val = person.address.state;
+ val = person.address['city'];
+ val = person.getBirthYear();
+
 console.log(val);
+
+const people = [
+  {name: 'John', age: 30},
+  {name: 'Mike', age: 23},
+  {name: 'Nancy', age: 40}
+];
+
+for(let i = 0; i < people.length; i++){
+  console.log(people[i].name);
+}
+  
+// let val;
+
+const today = new Date();
+let birthday = new Date('9-10-1981 11:25:00');
+birthday = new Date('September 10 1981');
+birthday = new Date('9/10/1981');
+
+val = today.getMonth();//it is 0 based
+val = today.getDate();
+val = today.getDay();
+val = today.getFullYear();
+val = today.getHours();
+val = today.getMinutes();
+val = today.getSeconds();
+val = today.getMilliseconds();
+val = today.getTime();
+
+birthday.setMonth(2);
+birthday.setDate(12);
+birthday.setFullYear(1985);
+birthday.setHours(3);
+birthday.setMinutes(30);
+birthday.setTime(10);
+
+
+// val = today;
+// val = birthday;
+
+// console.log(val);
+console.log(birthday);
+
