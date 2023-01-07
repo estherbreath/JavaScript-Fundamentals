6de2ac4e95e7ea26f54b3addffc5460122c36651
estherbreath <estherojile248@gmail.com>
2023-01-07 17:20:48 +0100

    changes

--- script.js
+++ script.js
@@ -63,15 +63,74 @@
 
 
 // Numbers The Math Object
-const num1 = 100; 
-const num2 = 50;
+// const num1 = 100; 
+// const num2 = 50;
+// let val;
+
+// //Simple math with numbers
+// val = num1 + num2;
+// val = num1 * num2;
+// val = num1 - num2;
+// val = num1 / num2;
+// val = num1 % num2;
+// console.log(val);
+
+
+const firstName = 'William';
+const lastName = 'Johnson';
+const age = 36;
+const str = 'Hello there my name is Brad';
+const tags = 'web design, development, programming';
 let val;
 
-//Simple math with numbers
-val = num1 + num2;
-val = num1 * num2;
-val = num1 - num2;
-val = num1 / num2;
-val = num1 % num2;
+//concatenation
+val = firstName + ' ' + lastName
+
+//Append - To add to
+val = 'Brad ';
+val += 'Travery';
+
+val = 'Hello, my name is ' + firstName + ' and I am ' + age;
+
+//Escaping -it makes special character regular (')
+val = 'That\'s awesome, I can\'t wait';
+
+//Lenght
+val = firstName.length;
+
+//concat
+val = firstName.concat(' ', lastName)
+
+//Change case
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+
+val = firstName[2];
+//indexOf()
+val = firstName.indexOf('l');
+val = firstName.lastIndexOf('l');
+
+//charAt()
+val = firstName.charAt('2');
+//Get last char
+val = firstName.charAt(firstName.length - 1);
+
+//subsrting()
+val = firstName.substring(0,4);
+
+//slice()
+val = firstName.slice(0,4);
+val = firstName.slice(-3);
+
+//split()
+val = str.split(' ');
+val = tags.split(' , ');
+
+//replace()
+val = str.replace('Brad', 'Jack');
+
+//includes
+val = str.includes('foo');
+
 console.log(val);
 
