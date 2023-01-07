f2d8f35f3cad23da255b90ecb79116893c453af6
estherbreath <estherojile248@gmail.com>
2023-01-07 17:58:29 +0100

    changes

--- script.js
+++ script.js
@@ -458,62 +458,94 @@
 
 //PROPERTY METHOD
 
-const todo = {
-  add: function(){
-    console.log('Add todo..');
-  },
-  edit: function(id){
-    console.log(`Edit todo ${id}`);
-  }
-}
+// const todo = {
+//   add: function(){
+//     console.log('Add todo..');
+//   },
+//   edit: function(id){
+//     console.log(`Edit todo ${id}`);
+//   }
+// }
 
-todo.delete = function(){
-  console.log('Delete todo...');
-}
+// todo.delete = function(){
+//   console.log('Delete todo...');
+// }
 
-todo.add();
-todo.edit(22);
+// todo.add();
+// todo.edit(22);
 
-todo.delete();
+// todo.delete();
 
-// LOOPS AND ITERATION
-// FOR LOOP
- for(let i = 0; i <= 10; i++){
-  // console.log(i);
-  if(i ===2){
-    console.log('2 is my favorite number');
-    continue;
-  }
+// // LOOPS AND ITERATION
+// // FOR LOOP
+//  for(let i = 0; i <= 10; i++){
+//   // console.log(i);
+//   if(i ===2){
+//     console.log('2 is my favorite number');
+//     continue;
+//   }
 
-  if (i === 5){
-    console.log('Stop the loop');
-    break;
-  }
- console.log('Number ' + i);
-}
+//   if (i === 5){
+//     console.log('Stop the loop');
+//     break;
+//   }
+//  console.log('Number ' + i);
+// }
 
-// WHILE LOOP
+// // WHILE LOOP
+
+// // let i = 0;
+
+// while(i <= 10){
+//   console.log('Number ' + i);
+//   i++;
+// }
+
+// // DO WHILE
 
 // let i = 0;
 
-while(i <= 10){
-  console.log('Number ' + i);
-  i++;
+// do{
+//   console.log('Number ' + i);
+//   i++;
+// }
+// while(i <= 10);
+
+// // LOOP THROUGH ARRAY
+// const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
+
+// for(let i = 0; i < cars.length; i++){
+//   console.log(cars[i]);
+// }
+
+//FOREACH
+cars.forEach(function(car, index, array){
+  console.log(`${index} : ${car}`);
+  console.log(array);
+});
+
+
+MAP  - (Revisit)
+const users = [
+  {id: 1, name: 'John'},
+  {id: 2, name: 'Sara'},
+  {id: 3, name: 'Karen'},
+  {id: 4, name: 'Steve'}
+];
+
+ const ids = users.map(function(user){
+    return user.ids;
+ });
+
+ console.log(ids);
+
+const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 40
 }
 
-// DO WHILE
-
-let i = 0;
-
-do{
-  console.log('Number ' + i);
-  i++;
-}
-while(i <= 10);
-
-// LOOP THROUGH ARRAY
-const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
-
-for(let i = 0; i < cars.length; i++){
-  console.log(cars[i]);
+for(let x in user){
+  // console.log(x);
+  console.log(`${x} : ${user[x]}`);
 }
