d549b64a089b8593bbe518cb2909de3c67e0f2bc
estherbreath <estherojile248@gmail.com>
2023-01-07 17:11:02 +0100

    changes

--- script.js
+++ script.js
@@ -16,14 +16,38 @@
 // console.timeEnd('Hello');
 
 //Variables
-var greeting;
-console.log(greeting);
-greeting = 'Hello';
-console.log(greeting);
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
 
-//Multi word vars
-var firstName = 'John'; //Camel case
-var first_name = 'Sara'; //Underscore
-var FirstName = 'Tom'; //Pascal case
-var firstname;
+// //Multi word vars
+// var firstName = 'John'; //Camel case
+// var first_name = 'Sara'; //Underscore
+// var FirstName = 'Tom'; //Pascal case
+// var firstname;
+
+let name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+// const name = 'John';
+// console.log(name);
+
+// const of object
+const person = {
+    name: 'John',
+    age: 30
+}
+person.name = 'Sara';
+person.age = 32;
+
+// console.log(person)
+
+// const of arrays
+const numbers = [1,2,3,4,5];
+numbers.push(6)
+
+console.log(numbers);
 
