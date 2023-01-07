006c1aa596a09cc53f220b1c4beff2e9dc58d00d
estherbreath <estherojile248@gmail.com>
2023-01-07 17:47:32 +0100

    changes

--- script.js
+++ script.js
@@ -365,24 +365,68 @@
 
 
 // LOGICAL OPERATORS
-const name = 'Steve';
-const age = 8;
+// const name = 'Steve';
+// const age = 8;
 
-//AND &&
-if(age > 0 && age < 12){
-  console.log(`${name} is a child`);
-}else if(age >= 13 && age <= 19){
-  console.log(`${name} is a teenager`);
-}else{
-  console.log(`${name} is an adult`);
+// //AND &&
+// if(age > 0 && age < 12){
+//   console.log(`${name} is a child`);
+// }else if(age >= 13 && age <= 19){
+//   console.log(`${name} is a teenager`);
+// }else{
+//   console.log(`${name} is an adult`);
+// }
+
+// // OR ||
+// if(age < 16 || age >65){
+//   console.log(`${name} can not run in race`);
+// }else{
+//   console.log(`${name} is registered for the race`);
+// }
+
+// //TENAARY OPERATOR (short cut)
+// console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
+
+
+// SWITCHES
+const color = 'yellow';
+
+switch(color){
+  case 'red':
+    console.log('Color is red');
+    break;
+  case 'blue':
+    console.log('Color is blue');
+    break;
+  default:
+      console.log('Color is not red or blue');
+      break;
 }
 
-// OR ||
-if(age < 16 || age >65){
-  console.log(`${name} can not run in race`);
-}else{
-  console.log(`${name} is registered for the race`);
-}
+let day;
 
-//TENAARY OPERATOR (short cut)
-console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
+switch(new Date().getDay()){
+  case 0:
+    day = 'Sunday';
+    break;
+  case 1:
+    day = 'Monday';
+    break;
+  case 2:
+     day = 'Tueday';
+     break;
+  case 3:
+      day = 'Wednesday';
+      break;
+  case 4:
+       day = 'Thursday';
+       break;
+  case 5:
+       day = 'Friday';
+       break;
+  case 6:
+      day = 'Saturday';
+      break; 
+   
+}
+console.log(`Today is ${day}`);
